c0ca6232c90a63add0f13c77a1cd4e8a92c7679c
Kirill Diordiev <diordiev@kodors.co>
2022-02-03 14:46:18 +0300

    KD tree accelerator declaration WIP

--- include/algorithms/kdtree.h
+++ include/algorithms/kdtree.h
@@ -7,16 +7,43 @@
 
 #include "bounds.h"
 
-struct kd_tree_node;
+struct primitive;
+struct prim_info;
+
+typedef struct {
+    union {
+        int flags;
+        int n_prims;
+        int above_child;
+    };
+} kd_tree_node;
+
+enum kd_tree_edge_type { Start, End };
+
+typedef struct  {
+    float t;
+    int prim_num;
+    enum kd_tree_edge_type type;
+} bound_edge;
+
 struct bound;
 
 struct kdtree_tree {
-    int intersect_cost,
-    traversal_cost, max_prims;
+    int intersect_cost, traversal_cost,
+            max_prims;
+    struct primitive *primitives;
+    int *primitive_indices;
     kd_tree_node *nodes;
     int n_alloced_nodes, next_free_node;
-
-
+    bounds3 bounds;
+    struct prim_info;
 };
+
+struct kd_tree_to_do {
+    kd_tree_node *node;
+    float t_min, t_max;
+};
+
+void algo_create_kd_tree(void *prims, struct prim_info *info);
 
 #endif //ALGORITHMS_KDTREE_H
---
+++ src/kdtree.c
@@ -0,0 +1,5 @@
+//
+// Created by kodor on 2/3/22.
+//
+
+#include "algorithms/kdtree.h"
